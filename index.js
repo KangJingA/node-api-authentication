4c4b84f7202c8dbf3f5f62665044e6c15a41c589
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-05-02 19:32:53 +0800

    login

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+// import routes
+const authRoute = require("./routes/auth");
+
+// grab data from .env
 dotenv.config();
+
 // connect to DB
 const uri = process.env.uri;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
   console.log("connected to DB")
 );
 
-// import routes
-const authRoute = require("./routes/auth");
+// middleware
 
-app.get("/", (req, res) => {
-  res.send("this is working");
-});
+// only for PUT and POST requests
+// to recognize the incoming Request Object as a JSON Object 
+app.use(express.json());
 
 // route middleware
 app.use("/api/user", authRoute);
+
+app.get("/", (req, res) => {
+    res.send("this is working");
+  });
+  
 
 app.listen(3000, () => console.log("server is working"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,57 @@
 // routes for authentication
-// export it
-
 const router = require("express").Router();
+// grab schema
+const User = require("../model/User");
+// validation functions
+const { registerValidation, loginValidation } = require("../validation");
+// hashing
+const bcrypt = require("bcryptjs");
 
-router.post('/register', (req, res) => {
-  res.send("register");
+router.post("/register", async (req, res) => {
+  // validate data before creating user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // check if user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("email already exists");
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  // create user
+  // check out REST API video to understand this
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassword,
+  });
+  // store user
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (error) {
+    // set response status to 400, send error
+    res.status(400).send(error);
+  }
 });
 
+router.post("/login", async(req,res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne({ email: req.body.email }); // understand how to destructure
+  console.log(user);
+  if (!user) return res.status(400).send("Email does not exist");
+
+  // check password is correct
+  // compare body password vs password in database
+  const validPass = await bcrypt.compare(req.body.password, user.password)
+  if (!validPass) return res.status(400).send("Invalid password")
+  
+  res.send("log in success");
+})
 
 
 module.exports = router;
